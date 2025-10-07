8f8be79c89776ec636674356a027cb5168485545
br <learnloop365@gmail.com>
2025-10-07 19:00:18 -0400

    .

--- pdfs/writer.go
+++ pdfs/writer.go
@@ -22,24 +22,3 @@ type Writer[T any] interface {
 	WriteToFile(filepath string) error
 	ProduceBytes() ([]byte, error)
 }
-
-type CountWriter struct {
-	w io.Writer
-	n int64
-}
-
-func NewCountWriter(w io.Writer) *CountWriter {
-	return &CountWriter{w: w}
-}
-
-// Write implements io.Writer
-func (cw *CountWriter) Write(p []byte) (int, error) {
-	n, err := cw.w.Write(p)
-	cw.n += int64(n) // cuz Write() can be called multiple times internally
-	return n, err
-}
-
-// BytesWritten returns the total number of bytes written
-func (cw *CountWriter) BytesWritten() int64 {
-	return cw.n
-}
