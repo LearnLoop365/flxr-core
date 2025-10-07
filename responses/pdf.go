ab756daeeec2ee2a18972f7609a4ad9c0bee64df
br <learnloop365@gmail.com>
2025-10-07 18:45:17 -0400

    .

--- responses/pdf.go
+++ responses/pdf.go
@@ -7,11 +7,16 @@ import (
 )
 
 func WritePDFBytes(w http.ResponseWriter, filename string, PDFBytes []byte) {
-	w.Header().Set("Content-Type", "application/pdf")
-	w.Header().Set("Content-Disposition", fmt.Sprintf("inline; filename=%q", filename))
-	w.WriteHeader(http.StatusOK) // Response Header Sent & Frozen
+	WritePDFResponseHeaders(w, filename)
 	_, err := w.Write(PDFBytes)
 	if err != nil {
 		log.Printf("[ERROR] writing PDF to response: %v", err)
 	}
+}
+
+// WritePDFResponseHeaders write HTTP response headers for PDF response. i.e. headers are frozen
+func WritePDFResponseHeaders(w http.ResponseWriter, filename string) {
+	w.Header().Set("Content-Type", "application/pdf")
+	w.Header().Set("Content-Disposition", fmt.Sprintf("inline; filename=%q", filename))
+	w.WriteHeader(http.StatusOK) // Response Header Sent & Frozen
 }
