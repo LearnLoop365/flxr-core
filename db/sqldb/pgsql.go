9251c258bb99c704890ae9ddf349762059e8fe29
br <learnloop365@gmail.com>
2025-10-10 12:41:21 -0400

    .

---
+++ db/sqldb/pgsql.go
@@ -0,0 +1,68 @@
+package sqldb
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib" // pgx stdlib driver for database/sql
+)
+
+// PgsqlClient implements SQLDBClient
+type PgsqlClient struct {
+	Conf *Conf
+
+	// internal fields are implementation details, not exported
+	db  *sql.DB
+	dsn string
+}
+
+func (c *PgsqlClient) Init() error {
+	var err error
+
+	// DSN format for pgx (URL or key/value style)
+	// Note: sslmode=disable is often used for local dev, adjust as needed.
+	// PostgreSQL natively allows multiple statements in a single query string.
+	c.dsn = fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=disable&timezone=%s",
+		c.Conf.User,
+		c.Conf.PW,
+		c.Conf.Host,
+		c.Conf.Port,
+		c.Conf.DB,
+		c.Conf.TZ,
+	)
+	// or key/value DSN format (also supported):
+	// c.dsn = fmt.Sprintf(
+	//    "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable TimeZone=%s",
+	//     c.Conf.Host, c.Conf.Port, c.Conf.User, c.Conf.PW, c.Conf.DB, c.Conf.TZ)
+
+	c.db, err = sql.Open("pgx", c.dsn)
+	if err != nil {
+		return err
+	}
+
+	// connection settings
+	c.db.SetConnMaxLifetime(3 * time.Minute)
+	c.db.SetMaxOpenConns(10)
+	c.db.SetMaxIdleConns(10)
+
+	if err = c.db.Ping(); err != nil {
+		return fmt.Errorf("postgres ping failed: %w", err)
+	}
+
+	log.Println("[INFO] pgsql client initialized")
+	return nil
+}
+
+func (c *PgsqlClient) Close() error {
+	if c.db == nil {
+		return nil
+	}
+	return c.db.Close()
+}
+
+func (c *PgsqlClient) DBHandle() *sql.DB {
+	return c.db
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.25.2
 require (
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/jackc/pgx/v5 v5.7.6
+	github.com/phpdave11/gofpdf v1.4.3
 	github.com/redis/go-redis/v9 v9.14.0
 	golang.org/x/crypto v0.42.0
 )
@@ -13,5 +15,12 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/phpdave11/gofpdi v1.0.15 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 )
